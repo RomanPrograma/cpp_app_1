7dccab7d49c88269c039f983da9fa1ba3febd0e7
Roman <roma2000kuba@gmail.com>
2023-05-12 12:50:23 +0300

    Add project files.

---
+++ cpp_app_1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Name        : \"" << “The war and the peace” << "\"" << endl;
+    cout << "Avtor       : " << L.N.Tolstoj << endl;
+    cout << "Izdatelstvo : " << Piter << endl;
+    cout << "Pages       : " << 500. << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
